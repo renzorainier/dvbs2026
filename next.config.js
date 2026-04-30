9fe28f930b9645e97642ed4d1715894e0297e473
renzorainier <114910204+renzorainier@users.noreply.github.com>
2026-04-30 23:29:04 +0800

    Fix Next.js 16 config and disable Turbopack

--- next.config.js
+++ next.config.js
@@ -1,33 +1,43 @@
 const withPWA = require('next-pwa')({
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development', // prevent warning in dev
-  });
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
 
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['openweathermap.org'],
-    },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 
-    webpack: (config, { isServer }) => {
-      config.module.rules.push({
-        test: /\.(wav|mp3|ogg|mp4)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              publicPath: '/_next/static/sounds/',
-              outputPath: 'static/sounds/',
-              name: '[name].[ext]',
-              esModule: false,
-            },
+  // This explicitly allows your Codespace preview to connect
+  allowedDevOrigins: ['127.0.0.1', '10.0.2.54', 'localhost'],
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'openweathermap.org',
+        pathname: '/**',
+      },
+    ],
+  },
+
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(wav|mp3|ogg|mp4)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next/static/sounds/',
+            outputPath: 'static/sounds/',
+            name: '[name].[ext]',
+            esModule: false,
           },
-        ],
-      });
+        },
+      ],
+    });
 
-      return config;
-    },
-  };
+    return config;
+  },
+};
 
-  module.exports = withPWA(nextConfig);
+module.exports = withPWA(nextConfig);
